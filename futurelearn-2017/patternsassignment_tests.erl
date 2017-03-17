44d77b015e7ad2fa2dbedc9f73d1022d5f833d90
cstrobbe <c_strobbe-github@yahoo.co.uk>
2017-03-17 18:45:12 +0100

    pattnersassignment: more tests.

--- futurelearn-2017/patternsassignment_tests.erl
+++ futurelearn-2017/patternsassignment_tests.erl
@@ -8,16 +8,42 @@
 %% To run all the tests, simply run 'patternsassignment_tests:test().' 
 %% (The 'test()' function is automatically generated.)
 
-bits_test() ->
-  1 = patternsassignment:bits(1),     %% 0
-  2 = patternsassignment:bits(2),     %% 1
-  2 = patternsassignment:bits(3),     %% 11
-  3 = patternsassignment:bits(4),     %% 100
-  3 = patternsassignment:bits(7),     %% 111
-  4 = patternsassignment:bits(8),     %% 1000
-  4 = patternsassignment:bits(9),     %% 1001
-  4 = patternsassignment:bits(15),    %% 1111
-  5 = patternsassignment:bits(16),    %% 10000
-  6 = patternsassignment:bits(33),    %% 10 0001
-  7 = patternsassignment:bits(66).    %% 100 0010
+
+perimeter_test() ->
+  18.84955592153876 = patternsassignment:perimeter({circle, {0,0}, 3}),
+  31.415926535897932384626433832795 = patternsassignment:perimeter({circle, {-1,-1}, 5}),
+  4 = patternsassignment:perimeter({rectangle, {0,0}, 1,1}),
+  8 = patternsassignment:perimeter({square, {0,0}, 2}),
+  6 = patternsassignment:perimeter({triangle, {0,0}, 2,2,2}).
+
+area_test() ->
+  28.274333882308139146163790449516 = patternsassignment:area({circle, {0,0}, 3}),
+  4 = patternsassignment:area({square, {0,0}, 2}),
+  1 = patternsassignment:area({rectangle, {0,0}, 1,1}),
+  6.0 = patternsassignment:perimeter({triangle, {0,0}, 3,4,5}).
+
+%% sumbitsdirect_test() ->
+%%   3 = patternsassignment:sumbitsdirect(7),
+%%   1 = patternsassignment:sumbitsdirect(8),
+%%   4 = patternsassignment:sumbitsdirect(15).
+
+
+%% sumbits_test() ->
+%%   3 = patternsassignment:sumbits(7),
+%%   1 = patternsassignment:sumbits(8),
+%%   4 = patternsassignment:sumbits(15).
+
+
+countbits_test() ->
+  1 = patternsassignment:countbits(1),     %% 0
+  2 = patternsassignment:countbits(2),     %% 1
+  2 = patternsassignment:countbits(3),     %% 11
+  3 = patternsassignment:countbits(4),     %% 100
+  3 = patternsassignment:countbits(7),     %% 111
+  4 = patternsassignment:countbits(8),     %% 1000
+  4 = patternsassignment:countbits(9),     %% 1001
+  4 = patternsassignment:countbits(15),    %% 1111
+  5 = patternsassignment:countbits(16),    %% 10000
+  6 = patternsassignment:countbits(33),    %% 10 0001
+  7 = patternsassignment:countbits(66).    %% 100 0010
 
